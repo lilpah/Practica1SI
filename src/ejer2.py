7dca32a90847e91f2ea43fb0a1d673b4f4fae649
MiguelSanMiguel <126871628+MiguelSanMiguel@users.noreply.github.com>
2024-03-17 18:15:01 +0100

    Apartado b

--- src/ejer2.py
+++ src/ejer2.py
@@ -1,4 +1,6 @@
 import sqlite3
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
 from numpy import nan
@@ -15,13 +17,18 @@ print(f"Número de muestras: {len(usersDataFrame.dropna())}")
 
 
 # Apartado b: Media y desviación estándar del total de fechas en las que se ha cambiado la contraseña.
-
 datesDataFrame = pd.read_sql_query("SELECT username, dates FROM users", con)
-#datesDataFrame['dates'] = pd.to_datetime(datesDataFrame['dates'])
-datesDataFrameByUser = datesDataFrame.groupby('username')['dates'].nunique()
-datesMean = datesDataFrame.mean()
-datesDes = datesDataFrame.std()
+datesDataFrame['numDates'] = 0
+i = 0
+for str in datesDataFrame['dates']:
+    str = str.strip('[]').split(", ")
+    datesDataFrame.loc[i,'numDates'] = len(str)
+    i+=1
+
+print(datesDataFrame)
+datesMean = datesDataFrame['numDates'].mean()
+dateDv = datesDataFrame['numDates'].std()
 print(f'Media de cambios de contraseña: {datesMean}')
-print(f'Desviación estándar de cambios de contraseña: {datesDes}')
+print(f'Desviación estándar de cambios de contraseña: {dateDv}')
 
 con.close()
