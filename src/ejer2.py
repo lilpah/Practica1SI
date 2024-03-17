4008b9b6295e045ed13d8f30e84dfc33534617a4
ch0ve <naachovelazquez2003@gmail.com>
2024-03-17 18:27:05 +0100

    apartado c y d

--- src/ejer2.py
+++ src/ejer2.py
@@ -25,10 +25,34 @@ for str in datesDataFrame['dates']:
     datesDataFrame.loc[i,'numDates'] = len(str)
     i+=1
 
-print(datesDataFrame)
+
 datesMean = datesDataFrame['numDates'].mean()
 dateDv = datesDataFrame['numDates'].std()
 print(f'Media de cambios de contraseña: {datesMean}')
 print(f'Desviación estándar de cambios de contraseña: {dateDv}')
+
+
+# Apartado c: Media y desviación estándar del total de IPs que se han detectado
+ipsDataFrame = pd.read_sql_query("SELECT username, ips FROM users", con)
+ipsDataFrame['numIps'] = 0
+i = 0
+for str in ipsDataFrame['ips']:
+    str = str.strip('[]').split(", ")
+    ipsDataFrame.loc[i,'numIps'] = len(str)
+    i+=1
+
+ipsMean = ipsDataFrame['numIps'].mean()
+ipsDv = ipsDataFrame['numIps'].std()
+print(f'Media de IPs detectadas: {ipsMean}')
+print(f'Desviación estándar de IPs detectadas: {ipsDv}')
+
+
+# Apartado d: Media y desviación estándar del número de email recibidos de phishing en los que ha interactuado cualquier usuario.
+phishingDataFrame = pd.read_sql_query("SELECT username, phishingEmails FROM users", con)
+
+phishingMean = phishingDataFrame['phishingEmails'].mean()
+phishingDv = phishingDataFrame['phishingEmails'].std()
+print(f'Media de correos de phishing: {phishingMean}')
+print(f'Desviación estándar de correos de phishing: {phishingDv}')
 
 con.close()
