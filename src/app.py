4dd32972b880cd0468a72cd4d907377591b79baa
ch0ve <naachovelazquez2003@gmail.com>
2024-04-18 15:42:05 +0200

    ejercico2

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 DEVELOPMENT_ENV = True
 
@@ -30,9 +30,15 @@ def service():
     return render_template("service.html", app_data=app_data)
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", app_data=app_data)
+@app.route("/ejercicio2")
+def ejercicio2():
+    return render_template("ejercicio2.html", app_data=app_data)
+
+@app.route("/topXcriticalUsers",methods = ['POST'])
+def topXcriticalUsers():
+    if request.method == 'POST':
+        number = request.form['number']
+        return render_template('topXcriticalUsers.html', number=number)
 
 
 
