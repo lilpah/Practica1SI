7d479c46289cf5cf825176b00e976e44e2642f73
lilpah <95379856+lilpah@users.noreply.github.com>
2024-04-25 02:13:20 +0200

    Modelos funcionando
    Falta implementarlos en un form

--- src/modelos.py
+++ src/modelos.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.impute import SimpleImputer
 from sklearn import tree
 import graphviz
-from sklearn.tree import export_graphviz
+from sklearn.tree import export_graphviz, DecisionTreeClassifier
 from subprocess import call
 
 
@@ -33,8 +33,8 @@ def linearRegresion():
     usersDataFrame['x'] = imputer.fit_transform(usersDataFrame[['x']])
 
     # Seleccionar la nueva característica X y la variable de destino y
-    X = usersDataFrame[['x']]
-    y = usersDataFrame['critical']
+    X = np.array(usersDataFrame[['x']])
+    y = np.array(usersDataFrame['critical'])
 
     # Dividir los datos en conjuntos de entrenamiento y prueba
     X_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=33)
@@ -45,35 +45,31 @@ def linearRegresion():
     # Entrenar el modelo con los datos de entrenamiento
     modelLinReg.fit(X_train, y_train)
 
-    # Inicializar y_pred
-    y_pred = modelLinReg.predict(x_test)
-
-    """
-    y_pred = []
-    # Asignar valores a y_pred según la pendiente
-    for val in x_test['x']:
-        if val < pendiente:
-            y_pred.append(0)
-        else:
-            y_pred.append(1)
-    """
-
-
+    """     
+    ############## Todo esto mostrado en la memoria ##############
     
-    # Printear la pendiente
-    print("Pendiente ", modelLinReg.coef_)
+    ####### Predecir
+    y_pred = modelLinReg.predict(x_test)
+    
+    ####### Calcular pendiente
+    m = modelLinReg.coef_
 
-    # Calcular el score de y_test y de y_pred
+    ####### Printear la pendiente
+    print("Pendiente ", m)
+
+    ####### Calcular el error cuadratico
     squarredError = mean_squared_error(y_test, y_pred)
     print("Mean squared error:", squarredError)
 
-    # Mostrar graficamente
+    ####### Mostrar graficamente
     plt.scatter(x_test, y_test, color="black")
     plt.plot(x_test, y_pred, color="blue", linewidth=3)
     plt.xticks(())
     plt.yticks(())
     plt.show()
+    """
 
+    return modelLinReg
 
 
 def decisionTree():
@@ -86,8 +82,8 @@ def decisionTree():
     con.close()
 
     #Dividie los datos
-    x = usersDataFrame[['totalEmails', 'phishingEmails', 'clickedEmails']]
-    y = usersDataFrame['critical']
+    x = np.array(usersDataFrame[['totalEmails', 'phishingEmails', 'clickedEmails']])
+    y = np.array(usersDataFrame['critical'])
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=33)
 
@@ -97,13 +93,17 @@ def decisionTree():
     #Entrenamos modelo
     decTree = decTree.fit(x_train, y_train)
 
-
+    """
+    ############## Todo esto mostrado en la memoria ##############
+    
+    ####### Predecir
     predY = decTree.predict(x_test)
 
-    # Calculamos la exactitud del modelo
+    ######## Calculamos la exactitud del modelo
     accuracy = accuracy_score(y_test, predY)
     print("Accuracy:", accuracy)
 
+    ####### Representacion grafica
     feature_names = ['totalEmails', 'phishingEmails', 'clickedEmails']
     class_names = ['non_critical', 'critical']
 
@@ -114,6 +114,9 @@ def decisionTree():
                                     special_characters=True)
     graph = graphviz.Source(dot_data)
     graph.render('criticidadUsuarios.gv', view=True).replace('\\', '/')
+    """
+
+    return decTree
 
 
 def randomForest():
@@ -126,8 +129,8 @@ def randomForest():
     con.close()
 
     # Dividie los datos
-    x = usersDataFrame[['totalEmails', 'phishingEmails', 'clickedEmails']]
-    y = usersDataFrame['critical']
+    x = np.array(usersDataFrame[['totalEmails', 'phishingEmails', 'clickedEmails']])
+    y = np.array(usersDataFrame['critical'])
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=33)
 
@@ -135,48 +138,94 @@ def randomForest():
     ranForest = RandomForestClassifier(max_depth=2, random_state=0,n_estimators=10)
     ranForest.fit(x_train, y_train)
 
-
-
-
-    # Predecir utilizando el conjunto de prueba
+    """
+    ############## Todo esto mostrado en la memoria ##############
+    
+    ####### Predecir utilizando el conjunto de prueba
     predY = ranForest.predict(x_test)
-    """
-    uda = [256, 65, 12]
-    uda_matriz = np.array([uda])
-    predY = ranForest.predict(uda_matriz)
-    print(uda_matriz)
-    print(predY)
 
-    """
-
-
-
-
-    # Calcular la precisión del modelo
+    ####### Calcular la precisión del modelo
     accuracy = accuracy_score(y_test, predY)
     print("Accuracy:", accuracy)
 
-
-    # Muestro todos los subarboles del forest en el directorio randomForest
+    ####### Muestro todos los subarboles del forest en el directorio randomForest
     feature_names = ['totalEmails', 'phishingEmails', 'clickedEmails']
     class_names = ['non_critical', 'critical']
     for i in range(len(ranForest.estimators_)):
         print(i)
         estimator = ranForest.estimators_[i]
         export_graphviz(estimator,
-                        out_file='tree.dot',
+                        out_file='randomTree/tree.dot',
                         feature_names=feature_names,
                         class_names=class_names,
                         rounded=True, proportion=False,
                         precision=2, filled=True)
         call(['dot', '-Tpng', 'tree.dot', '-o', 'randomTree/' + 'tree' + str(i) + '.png', '-Gdpi=600'])
+    """
+
+    return ranForest
+
+
+def prediccion(model, user):
+    if isinstance(model, LinearRegression):
+        # Realizar predicción utilizando el modelo de regresión lineal
+        predY = model.predict(user)
+        m = model.coef_
+
+        # Si la prediccion es menor que la pendiente -> usuario no critico
+        if predY < m:
+            return "El usuario no es critico"
+        else:
+            return "El usuario es critico"
+    elif isinstance(model, DecisionTreeClassifier):
+        # Realizar predicción utilizando el modelo de árbol de decisión
+        predY = model.predict(user)
+
+        # Si la prediccion es 1 -> usuario crtico
+        if predY == 1:
+            return "El usuario es critico"
+        else:
+            return "El usuario no es critico"
+    elif isinstance(model, RandomForestClassifier):
+        # Realizar predicción utilizando el modelo de bosque aleatorio
+        predY = model.predict(user)
+
+        # Si la prediccion es 1 -> usuario crtico
+        if predY == 1:
+            return "El usuario es critico"
+        else:
+            return "El usuario no es critico"
+    else:
+        print("Modelo no válido")
+        return None
 
 
 
 if __name__ == '__main__':
-    #linearRegresion()
-    decisionTree()
-    #randomForest()
 
+    # Modelo regresion lineal
+    dataRegresionLineal = [1.93]
+    dataRegresionLineal = np.array([dataRegresionLineal])
+    linReg = linearRegresion()
+
+    usuario1 = prediccion(linReg, dataRegresionLineal)
+    print(usuario1)
+
+
+    # Modelo decision tree
+    decision = decisionTree()
+    dataDecision = [101, 22, 14]
+    dataDecision = np.array([dataDecision])
+
+    usuario2 = prediccion(decision, dataDecision)
+    print(usuario2)
+
+    # Modelo random forest
+    random = randomForest()
+    dataRandom = [391, 22, 16]
+    dataRandom = np.array([dataRandom])
+
+    usuario3 = prediccion(random, dataRandom)
+    print(usuario3)
 
 
